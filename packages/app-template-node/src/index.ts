6b650f65f55e3f654014e15d657752f16d64b763
SamStarlling <samelabarbosa.06@gmail.com>
2023-05-09 15:41:51 -0300

    [refactor] Adjust position

--- packages/app-template-node/src/index.ts
+++ packages/app-template-node/src/index.ts
@@ -6,8 +6,8 @@ const port = process.env.PORT || 7200;
 
 const app = express();
 
-app.use(routes);
 app.use(cors());
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
