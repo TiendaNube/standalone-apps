f8d412c185b8bc66ed4253bc5b7ef1817c72e8ec
SamStarlling <samelabarbosa.06@gmail.com>
2023-05-10 10:15:21 -0300

    [feat] Check if has access_token and build header

--- packages/app-template-node/src/product/services/productService.ts
+++ packages/app-template-node/src/product/services/productService.ts
@@ -1,2 +1,27 @@
-class ProductService {}
+const jsonServer = require("json-server");
+const axios = require("axios");
+const database = jsonServer.router("db.json");
+import getCredentials from "../../utils/getCredentials.function";
+import sendApiResponse from "../../utils/sendApiResponse.function";
+import { StatusCode } from "./../../utils/statusCode.enum";
+class ProductService {
+  public async store(request:any, response:any) {
+    const credentials = getCredentials();
+
+    if(!credentials) {
+      return sendApiResponse(response, StatusCode.BAD_REQUEST, { message: "Authorization code not found" });
+    }
+
+  }
+
+  private buildHeaders(accessToken: string, ) {
+    return {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${accessToken}`,
+      "User-Agent": `My App (${process.env.USER_EMAIL})`,
+    }
+  }
+
+}
+
 module.exports = new ProductService();
