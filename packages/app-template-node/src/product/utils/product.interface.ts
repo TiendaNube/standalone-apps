c606b4bce5b6f481cd83d239c3d4c96a2f357e61
SamStarlling <samelabarbosa.06@gmail.com>
2023-05-10 17:35:15 -0300

    [feat] Add interface of product

---
+++ packages/app-template-node/src/product/utils/product.interface.ts
@@ -0,0 +1,14 @@
+import { faker } from "@faker-js/faker";
+
+export default interface Product {
+  images: [{ src: string }];
+  name: { en: string, pt: string, es: string};
+  variants: [ {
+    price: string;
+    stock_management: boolean;
+    stock: string;
+    weight: string;
+    cost: string;
+  }] ;
+  categories: [number];
+}
