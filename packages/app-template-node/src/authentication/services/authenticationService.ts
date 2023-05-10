403a631d819db6fb41cb9f7b872c5d5da8b77311
SamStarlling <samelabarbosa.06@gmail.com>
2023-05-10 10:17:59 -0300

    [feat] Rename method and use functions at utils

--- packages/app-template-node/src/authentication/services/authenticationService.ts
+++ packages/app-template-node/src/authentication/services/authenticationService.ts
@@ -1,6 +1,8 @@
 const jsonServer = require("json-server");
 const axios = require("axios");
 const database = jsonServer.router("db.json");
+import getCredentials from "../../utils/getCredentials.function";
+import sendApiResponse from "../../utils/sendApiResponse.function";
 import { StatusCode } from "./../../utils/statusCode.enum";
 
 class AuthenticationService {
@@ -8,25 +10,25 @@ class AuthenticationService {
     try {
       if(request.query.code) {
         const body = this.getBody(request.query.code);
-        const accessTokenResponse = await this.getAccessToken(body);
+        const accessTokenResponse = await this.authenticateApp(body);
 
         if(!accessTokenResponse.error){
           database.db.set("credentials", accessTokenResponse).write();
-          return this.sendApiResponse(response, StatusCode.OK, accessTokenResponse);
+          return sendApiResponse(response, StatusCode.OK, accessTokenResponse);
         }
 
-        return this.sendApiResponse(response, StatusCode.BAD_REQUEST, { message: accessTokenResponse.error_description });
+        return sendApiResponse(response, StatusCode.BAD_REQUEST, { message: accessTokenResponse.error_description });
       }
-      const credentials = this.getCredentials();
+      const credentials = getCredentials();
 
       if(!credentials) {
-        return this.sendApiResponse(response, StatusCode.BAD_REQUEST, { message: "Authorization code not found" });
+        return sendApiResponse(response, StatusCode.BAD_REQUEST, { message: "Authorization code not found" });
       }
 
-      return this.sendApiResponse(response, StatusCode.OK, credentials);
+      return sendApiResponse(response, StatusCode.OK, credentials);
     }
     catch(error: any) {
-      return this.sendApiResponse(response, StatusCode.INTERNAL_SERVER_ERROR, { message: error.message });
+      return sendApiResponse(response, StatusCode.INTERNAL_SERVER_ERROR, { message: error.message });
     }
   }
 
@@ -40,15 +42,7 @@ class AuthenticationService {
     }
   }
 
-  private getCredentials() {
-    return database.db.get("credentials").value();
-  }
-
-  private sendApiResponse(response:any, statusCode: number, message: {}) {
-    return response.status(statusCode).json(message);
-  }
-
-  private async getAccessToken(body: any) {
+  private async authenticateApp(body: any) {
     try {
       const response = await axios.post(process.env.AUTHENTICATION_API, body, {
         headers: {
