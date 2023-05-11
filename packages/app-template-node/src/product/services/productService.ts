f33eed68e55b84141ef3513fb2c3f00e464ce681
SamStarlling <samelabarbosa.06@gmail.com>
2023-05-10 23:00:54 -0300

    [feat] Create logic to insert 5 products random

--- packages/app-template-node/src/product/services/productService.ts
+++ packages/app-template-node/src/product/services/productService.ts
@@ -1,25 +1,52 @@
-const jsonServer = require("json-server");
 const axios = require("axios");
-const database = jsonServer.router("db.json");
 import getCredentials from "../../utils/getCredentials.function";
 import sendApiResponse from "../../utils/sendApiResponse.function";
+import generateProduct from "../utils/generateProduct.function";
+import Product from "../utils/product.interface";
 import { StatusCode } from "./../../utils/statusCode.enum";
 class ProductService {
-  public async store(request:any, response:any) {
-    const credentials = getCredentials();
+  public async insertFiveProducts(request:any, response:any) {
+    try {
+      const credentials = getCredentials();
 
-    if(!credentials) {
-      return sendApiResponse(response, StatusCode.BAD_REQUEST, { message: "Authorization code not found" });
+      if(!credentials.access_token) {
+        return sendApiResponse(response, StatusCode.BAD_REQUEST, { message: "Authorization code not found" });
+      }
+
+      let products: Product[] = [];
+      for(let index = 0; index < 4; index += 1) {
+        const product = generateProduct();
+
+        products.push
+          (await this.insertProducts(credentials.user_id, this.getHeader(credentials.access_token), product));
+      }
+
+      return sendApiResponse(response, StatusCode.CREATED, products);
+    } catch (error: any) {
+      return sendApiResponse(response, error.response.status, { message: error.message });
     }
-
   }
 
-  private buildHeaders(accessToken: string, ) {
+  private getHeader(accessToken: string) {
     return {
       "Content-Type": "application/json",
-      "Authorization": `Bearer ${accessToken}`,
+      "Authentication": `bearer ${accessToken}`,
       "User-Agent": `My App (${process.env.USER_EMAIL})`,
     }
+  }
+
+  private async insertProducts(storeId: number, header: any, body: any) {
+    const url = `${process.env.TIENDANUBE_API}${storeId}/products`;
+
+    return axios.post(url, body, {
+      headers: header,
+    })
+    .then((response: any) => {
+      return response.data;
+    })
+    .catch((error: any) => {
+      throw new Error(error);
+    });
   }
 
 }
