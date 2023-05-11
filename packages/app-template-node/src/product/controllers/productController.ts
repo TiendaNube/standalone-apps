653780d58a455f2468ae601eba848318a081a80a
SamStarlling <samelabarbosa.06@gmail.com>
2023-05-10 22:58:47 -0300

    [feat] Add store to insert 5 products at store

--- packages/app-template-node/src/product/controllers/productController.ts
+++ packages/app-template-node/src/product/controllers/productController.ts
@@ -1,5 +1,8 @@
 const productService = require("../services/productService");
 
 class ProductController {
+  async store(req: any, res: any) {
+    await productService.insertFiveProducts(req, res);
+  }
 }
 module.exports = new ProductController();
