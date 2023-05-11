8d6ac6240554739003e8853926ec8813dd2ce95d
SamStarlling <samelabarbosa.06@gmail.com>
2023-05-11 17:27:28 -0300

    [chore] Remove cors lib

--- packages/app-template-node/src/index.ts
+++ packages/app-template-node/src/index.ts
@@ -1,12 +1,10 @@
 const express = require("express");
 require("dotenv").config();
-const cors = require("cors");
 const routes = require("./routes");
 const port = process.env.PORT || 7200;
 
 const app = express();
 
-app.use(cors());
 app.use(routes);
 
 app.listen(port, () => {
