a42cd75b4f895cdbffd36ca1b19da28eee774190
SamStarlling <samelabarbosa.06@gmail.com>
2023-05-10 22:59:04 -0300

    [feat] New route to insert 5 products

--- packages/app-template-node/src/routes.ts
+++ packages/app-template-node/src/routes.ts
@@ -1,7 +1,9 @@
 const { Router } = require("express");
 const authenticationController = require("./authentication/controllers/authenticationController");
+const productController = require("./product/controllers/productController");
 
 const router = Router();
 router.get("/auth", authenticationController.find);
+router.post("/products", productController.store)
 
 module.exports = router;
