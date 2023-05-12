fc78259d8c05c88711ba21c911964051aae2be07
SamStarlling <samelabarbosa.06@gmail.com>
2023-05-11 23:37:44 -0300

    [refactor] Modify structure

--- packages/app-template-node/src/authentication/services/authenticationService.ts
+++ packages/app-template-node/src/authentication/services/authenticationService.ts
@@ -1,26 +1,41 @@
 const jsonServer = require("json-server");
 const axios = require("axios");
 const database = jsonServer.router("db.json");
-import sendApiResponse from "../../utils/sendApiResponse.function";
+import IAuthenticationResponse from "../../utils/authenticationResponse.interface";
+import IErrorResponse from "../../utils/errorResponse.interface";
 import { StatusCode } from "./../../utils/statusCode.enum";
 
 class AuthenticationService {
-  public async find(request: any, response: any) {
+  public async find(code: string): Promise<IAuthenticationResponse | IErrorResponse> {
     try {
-      if(!request.query.code) {
-        return sendApiResponse(response, StatusCode.BAD_REQUEST, { message: "The authorization code not found" });
+      if(!code) {
+        return {
+          statusCode: StatusCode.BAD_REQUEST,
+          error: "The authorization code not found",
+        }
       }
 
-      const body = this.getBody(request.query.code);
+      const body = this.getBody(code);
 
       const authenticateResponse = await this.authenticateApp(body);
 
-      database.db.set("credentials", authenticateResponse).write();
+      // This condition will be true when the code has been used or is invalid.
+      if(!authenticateResponse.data.access_token) {
+        return {
+          statusCode: StatusCode.BAD_REQUEST,
+          error: authenticateResponse.data.error_description,
+        }
+      }
 
-      return sendApiResponse(response, authenticateResponse.statusCode , authenticateResponse.data);
+      database.db.set("credentials", authenticateResponse.data).write();
+
+      return authenticateResponse;
     }
     catch(error: any) {
-      return sendApiResponse(response, StatusCode.INTERNAL_SERVER_ERROR, { message: error.message });
+      return {
+        statusCode: error.status,
+        data: error,
+      }
     }
   }
 
@@ -35,22 +50,17 @@ class AuthenticationService {
   }
 
   private async authenticateApp(body: any) {
-    try {
-      const response = await axios.post(process.env.AUTHENTICATION_API, body, {
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*", 
-        },
-      });
+    const response = await axios.post(process.env.AUTHENTICATION_API, body, {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*", 
+      },
+    });
 
-      return {
-        statusCode: response.status,
-        data: response.data,
-      };
-    }
-    catch(error: any) {
-      throw new Error(error);
-    }
+    return {
+      statusCode: response.status,
+      data: response.data,
+    };
   }
 }
 
