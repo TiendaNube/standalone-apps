6e690f5097172710c65bf16eebf8a378d766db25
SamStarlling <samelabarbosa.06@gmail.com>
2023-05-15 14:44:52 -0300

    [feat] Add route to get list of products

--- packages/app-template-node/src/routes.ts
+++ packages/app-template-node/src/routes.ts
@@ -5,7 +5,8 @@ const productController = require("./product/controllers/productController");
 const router = Router();
 router.get("/auth", authenticationController.find);
 router.post("/products", productController.store);
-router.get("/products", productController.index);
+router.get("/products/total", productController.getTotal);
+router.get("/products", productController.getAll);
 
 
 
