93256d1281e2e9e076ccd9d29e0f896165b4ca9e
SamStarlling <samelabarbosa.06@gmail.com>
2023-05-15 09:42:43 -0300

    [feat] Add new property at interface

--- packages/app-template-node/src/utils/credentials.interface.ts
+++ packages/app-template-node/src/utils/credentials.interface.ts
@@ -1,6 +1,7 @@
 export default interface ICredentials {
-  "access_token": string;
-  "token_type": string;
-  "scope": string;
-  "user_id": number;
+  access_token?: string;
+  token_type?: string;
+  scope?: string;
+  user_id?: number;
+  error_description?: string;
 }
