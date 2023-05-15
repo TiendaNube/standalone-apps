869a18873705e98d0e0f48ef47a3c57d917ddc48
SamStarlling <samelabarbosa.06@gmail.com>
2023-05-15 15:40:01 -0300

    [feat] New route to delete product

--- packages/app-template-node/src/routes.ts
+++ packages/app-template-node/src/routes.ts
@@ -4,9 +4,11 @@ const productController = require("./product/controllers/productController");
 
 const router = Router();
 router.get("/auth", authenticationController.find);
-router.post("/products", productController.store);
 router.get("/products/total", productController.getTotal);
 router.get("/products", productController.getAll);
+router.post("/products", productController.store);
+router.delete("/products/:id", productController.delete);
+
 
 
 
