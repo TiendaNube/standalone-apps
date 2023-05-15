025782183567d8e4208667cd49de18438d92a84a
SamStarlling <samelabarbosa.06@gmail.com>
2023-05-15 15:40:23 -0300

    [feat] New method to delete product

--- packages/app-template-node/src/product/controllers/productController.ts
+++ packages/app-template-node/src/product/controllers/productController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 const insertFiveProductsService = require("../services/insertFiveProductsService");
 const getTotalProductsService = require("../services/getTotalProductsService");
 const listAllProductsService = require("../services/listAllProductsService");
+const deleteProductsService = require("../services/deleteProductsService");
 class ProductController {
   // Insert 5 new products at store
   async store(req: Request, res: Response) {
@@ -19,6 +20,11 @@ class ProductController {
   async getAll(req: Request, res: Response) {
     const { statusCode, data } = await listAllProductsService.findAll();
     return res.status(statusCode).json(data);
+  }
+
+  async delete(req: Request, res: Response) {
+    const { statusCode, data } = await deleteProductsService.deleteById(req.params.id);
+    return !data ? res.status(statusCode).json(req.params.id) : res.status(statusCode).json(data);
   }
 }
 module.exports = new ProductController();
