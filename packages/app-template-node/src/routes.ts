7d49f37a2bfd7bb9f670adcb250922de3fb17f54
SamStarlling <samelabarbosa.06@gmail.com>
2023-05-15 14:12:37 -0300

    [feat] New route to get total of products

--- packages/app-template-node/src/routes.ts
+++ packages/app-template-node/src/routes.ts
@@ -4,6 +4,9 @@ const productController = require("./product/controllers/productController");
 
 const router = Router();
 router.get("/auth", authenticationController.find);
-router.post("/products", productController.store)
+router.post("/products", productController.store);
+router.get("/products", productController.index);
+
+
 
 module.exports = router;
