d72dd2df2c5b5923da30db135fd25bb6a96116d5
SamStarlling <samelabarbosa.06@gmail.com>
2023-05-15 14:13:13 -0300

    [feat] New method to get total products

--- packages/app-template-node/src/product/controllers/productController.ts
+++ packages/app-template-node/src/product/controllers/productController.ts
@@ -1,10 +1,17 @@
 import { Request, Response } from "express";
 const insertFiveProductsService = require("../services/insertFiveProductsService");
-
+const getTotalProductsService = require("../services/getTotalProductsService");
 class ProductController {
+  // Insert 5 new products at store
   async store(req: Request, res: Response) {
     const { statusCode, data } = await insertFiveProductsService.store();
 
+    return res.status(statusCode).json(data);
+  }
+
+  // Get total products at store
+  async index(req: Request, res: Response) {
+    const { statusCode, data } = await getTotalProductsService.findAll();
     return res.status(statusCode).json(data);
   }
 }
