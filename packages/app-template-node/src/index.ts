b7c0e7f883526900917ff470bd12ebecc679425b
SamStarlling <samelabarbosa.06@gmail.com>
2023-05-16 12:25:49 -0300

    [feat] Add middleware to disable cors

--- packages/app-template-node/src/index.ts
+++ packages/app-template-node/src/index.ts
@@ -3,7 +3,9 @@ require("dotenv").config();
 const routes = require("./routes");
 const port = process.env.PORT || 7200;
 const app = express();
+const cors = require("cors");
 
+app.use(cors());
 app.use(routes);
 
 app.listen(port, () => {
