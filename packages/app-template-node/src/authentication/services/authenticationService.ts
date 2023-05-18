9123959c275ee2f92cffda02291ccd05290076f1
SamStarlling <samelabarbosa.06@gmail.com>
2023-05-18 01:41:51 -0300

    [refactor] Change structure of code

--- packages/app-template-node/src/authentication/services/authenticationService.ts
+++ packages/app-template-node/src/authentication/services/authenticationService.ts
@@ -1,16 +1,15 @@
-const jsonServer = require("json-server");
-const axios = require("axios");
-const database = jsonServer.router("db.json");
+import axios from "axios";
 import IAuthenticationResponse from "../../utils/authenticationResponse.interface";
 import { BodyAuthenticationType } from "../../utils/body.type";
-import IErrorResponse from "../../utils/errorResponse.interface";
 import { StatusCode } from "./../../utils/statusCode.enum";
+import { getCredentials, setCredentials } from "../../utils/jsonServerConfig";
+import ICredentials from "../../utils/credentials.interface";
 
 class AuthenticationService {
   public async find(code: string): Promise<IAuthenticationResponse> {
     try {
       if(!code) {
-        const credentials = database.db.get("credentials").value();
+        const credentials = getCredentials();
         if (credentials) {
           return {
             statusCode: StatusCode.OK,
@@ -21,18 +20,10 @@ class AuthenticationService {
           statusCode: StatusCode.BAD_REQUEST,
           data: "The authorization code not found",
         }
-      }
-
-      if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET) {
-        return {
-          statusCode: StatusCode.BAD_REQUEST,
-          data: "Its necessary set request variables at .env-example file and rename to .env file",
-        }
       } 
-
       const body: BodyAuthenticationType = {
-        client_id: process.env.CLIENT_ID,
-        client_secret: process.env.CLIENT_SECRET,
+        client_id: process.env.CLIENT_ID as string,
+        client_secret: process.env.CLIENT_SECRET as string,
         grant_type: "authorization_code",
         code: code,
       }
@@ -40,18 +31,19 @@ class AuthenticationService {
       const authenticateResponse = await this.authenticateApp(body);
 
       // This condition will be true when the code has been used or is invalid.
-      if(authenticateResponse.data.error && authenticateResponse.data.error_description) {
+      if(authenticateResponse.error && authenticateResponse.error_description) {
         return {
           statusCode: StatusCode.BAD_REQUEST,
-          data: authenticateResponse.data.error_description,
+          data: authenticateResponse.error_description,
         }
       }
 
-      database.db.set("credentials", authenticateResponse.data).write();
+      // Insert response of Authentication API at config.json file
+      setCredentials(authenticateResponse);
 
       return {
         statusCode: StatusCode.OK,
-        data: authenticateResponse.data,
+        data: authenticateResponse,
       }
     } catch (error: any) {
       return {
@@ -62,13 +54,16 @@ class AuthenticationService {
   }
 
 
-  private async authenticateApp(body: any): Promise<any> {
-    return await axios.post(process.env.AUTHENTICATION_API, body, {
+  private async authenticateApp(body: BodyAuthenticationType): Promise<ICredentials> {
+    return await axios.post(process.env.AUTHENTICATION_API as string, body, {
       headers: {
         "Content-Type": "application/json",
       },
-    });
+    })
+    .then((response: any): ICredentials => {
+      return response.data;
+    })
   }
 }
 
-module.exports = new AuthenticationService();
+export default new AuthenticationService();
