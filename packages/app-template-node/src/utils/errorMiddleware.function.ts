e10eb91289f1f7a28c4d5a22b7fea3755b3d4e65
SamStarlling <samelabarbosa.06@gmail.com>
2023-05-18 18:18:56 -0300

    [fix] Adjust style code

--- packages/app-template-node/src/utils/errorMiddleware.function.ts
+++ packages/app-template-node/src/utils/errorMiddleware.function.ts
@@ -4,8 +4,7 @@ import { StatusCode } from "./statusCode.enum";
 
 export default function errorMiddleware(req: Request, res: Response, next: NextFunction) {
   if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.AUTHENTICATION_API) {
-    return res.status(StatusCode.BAD_REQUEST).json("It is necessary to set request variables at .env-example file and rename it to .env",
-    );
+    return res.status(StatusCode.BAD_REQUEST).json("It is necessary to set request variables at .env-example file and rename it to .env");
   }
   const credentials = getCredentials();
 
