dc595a2bab36e62c4985dbd3e8d873e10e1ecb3f
SamStarlling <samelabarbosa.06@gmail.com>
2023-05-18 14:43:50 -0300

    [feat] Rename middleware error file

---
+++ packages/app-template-node/src/utils/errorMiddleware.function.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from "express";
+import { getCredentials } from "./jsonServerConfig";
+import { StatusCode } from "./statusCode.enum";
+
+export default function errorMiddleware(req: Request, res: Response, next: NextFunction) {
+  if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.AUTHENTICATION_API) {
+    return res.status(StatusCode.BAD_REQUEST).json("It is necessary to set request variables at .env-example file and rename it to .env",
+    );
+  }
+  const credentials = getCredentials();
+
+  if (req.path === "/products") {     
+    // Get the value of the "Authentication" header
+    const authenticationHeader = req.headers["authentication"] as string;
+
+    console.log("AUTHENTICATION HEADER: ", authenticationHeader);
+
+    //Check if the value of the "Authentication" header starts with "bearer" and return the access_token
+    const accessTokenHeader = authenticationHeader?.startsWith("bearer") ? authenticationHeader.slice(7) : undefined;
+
+    if(accessTokenHeader !== credentials.access_token) {
+      return res.status(StatusCode.BAD_REQUEST).json("The access_token of the Authentication is invalid");
+    }
+  }
+
+  next();
+}
+
