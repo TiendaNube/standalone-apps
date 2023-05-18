98f5e3f13809e6fba399ead31ddb21d4976459a5
SamStarlling <samelabarbosa.06@gmail.com>
2023-05-18 18:17:41 -0300

    [refactor] Change way to set headers

--- packages/app-template-node/src/product/services/insertFiveProductsService.ts
+++ packages/app-template-node/src/product/services/insertFiveProductsService.ts
@@ -1,4 +1,4 @@
-const axios = require("axios");
+import axios from "axios";
 import ICredentials from "../../utils/credentials.interface";
 import IHeaders from "../../utils/headers.interface";
 import generateProduct from "../utils/generateProduct.function";
@@ -6,6 +6,7 @@ import IProduct from "../utils/product.interface";
 import IProductResponse from "../utils/productResponse.interface";
 import { StatusCode } from "../../utils/statusCode.enum";
 import { getCredentials } from "../../utils/jsonServerConfig";
+import getHeaders from "../utils/getHeaders.function";
 
 class InsertFiveProductsService {
   public async store(): Promise<IProductResponse> {
@@ -14,8 +15,9 @@ class InsertFiveProductsService {
       let products:number[] = [];
       for(let index = 0; index < 5; index += 1) {
         const randomProduct: IProduct = generateProduct();
-      
-        const product = await this.insertProduct(credentials.user_id as number, this.getHeader(credentials.access_token as string), randomProduct);
+
+        const product = await this.insertProduct(credentials.user_id as number, credentials.access_token as string, randomProduct);
+
 
         products.push(product);
       }
@@ -43,20 +45,17 @@ class InsertFiveProductsService {
     }
   }
 
-  private getHeader(accessToken: string): IHeaders {
-    return {
-      "Content-Type": "application/json",
-      "Authentication": `bearer ${accessToken}`,
-      "User-Agent": `${process.env.APP_NAME} (${process.env.USER_EMAIL})`,
-    }
-  }
-
-  private async insertProduct(storeId: number, headers: IHeaders
+  private async insertProduct(storeId: number, accessToken: string
   , body: IProduct){
-    const url = `${process.env.TIENDANUBE_API}${storeId}/products`;
+    const url = `${process.env.AUTHENTICATION_API}/${storeId}/products`;
+    const header = getHeaders(accessToken);
 
     return axios.post(url, body, {
-      headers,
+      headers: {
+        "Content-Type": header["Content-Type"],
+        "Authentication": header.Authentication,
+        "User-Agent": header["User-Agent"],
+      },
     })
     .then((response: any) => {
       return response.data.id
