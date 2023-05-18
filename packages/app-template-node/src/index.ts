2a4020e1b602c1fdd9121bc8f9553d915d4ea95e
SamStarlling <samelabarbosa.06@gmail.com>
2023-05-18 01:36:32 -0300

    [feat] Add middleware to check variables at .env

--- packages/app-template-node/src/index.ts
+++ packages/app-template-node/src/index.ts
@@ -1,14 +1,20 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import dotenv from "dotenv"; 
 dotenv.config();
 import routes from "./routes";
 import cors from "cors";
+import envCheck from "./utils/envCheck.function";
 
 
 const port = process.env.PORT || 7200;
 const app = express();
 
+// Disable cors
 app.use(cors());
+
+// Check if all variables at .env file were set
+app.use(envCheck);
+
 app.use(routes);
 
 app.listen(port, () => {
