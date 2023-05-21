0b9850801f1ebb65908b998417746179910ad509
SamStarlling <samelabarbosa.06@gmail.com>
2023-05-20 23:49:53 -0300

    [chore] Add config to process.env

--- packages/app-template-vite/vite.config.ts
+++ packages/app-template-vite/vite.config.ts
@@ -1,3 +1,4 @@
+import dotenv from "dotenv";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
@@ -13,6 +14,7 @@ export default defineConfig({
   },
   define: {
     global: {},
+    "process.env": dotenv.config({ path: "../../.env.local" }).parsed,
   },
   resolve: {
     alias: {
