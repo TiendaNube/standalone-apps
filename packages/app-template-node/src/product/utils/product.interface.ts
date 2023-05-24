2db3bb978eeb3bdda4aa6863319de8612e4c4884
SamStarlling <samelabarbosa.06@gmail.com>
2023-05-24 20:59:03 -0300

    [refactor] Remove variants property

--- packages/app-template-node/src/product/utils/product.interface.ts
+++ packages/app-template-node/src/product/utils/product.interface.ts
@@ -1,11 +1,4 @@
 export default interface IProduct {
   images: [{ src: string }];
   name: { en: string, pt: string, es: string};
-  variants: [ {
-    price: string;
-    stock_management: boolean;
-    stock: string;
-    weight: string;
-    cost: string;
-  }];
 }
